efc919b5863ce50df9082e0c42b6ff222d8b6757
Surathep_Wongthepniwat <surathep2022@gmail.com>
2026-01-06 17:03:57 +0700

    update Page ShowHighlightList

--- corporate-Frontend/src/components/solution/datawarehouse/Information/highlight.js
+++ corporate-Frontend/src/components/solution/datawarehouse/Information/highlight.js
@@ -1,206 +1,159 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import Axios from "axios";
-import useHeader from "../../../hook/useHeader";
-import Hilight from "./highlightdata";
+import ShowHighlight from "./showhighlight";
 
-export default function HighlightExhibition(props) {
-  const url = process.env.REACT_APP_API_URI + process.env.REACT_APP_exh;
-  const bearer = useHeader();
-  Axios.defaults.headers.common = {
-    Authorization: "Bearer " + bearer,
-  };
-
-  const { BlobServiceClient } = require("@azure/storage-blob");
-
-  const [logo, setLogo] = useState({});
- 
+export default function CreateExhibition() {
+  const url = process.env.REACT_APP_API_URI + process.env.REACT_APP_web;
 
   const initData = {
-    Title: "",
-    Text: "",      
-    Showdate: "",
-  };  
-  const [data, setData] = useState(initData);
-  console.log(data);
+    title: "",
+    showdate: "",
+    description: "",
+  };
 
+  const [data, setData] = useState(initData);
+  const [file, setFile] = useState();
+  const [reloadFlag, setReloadFlag] = useState(0); // 🔁 trigger reload
+
+  // 🔹 Clear form
   const clickCancel = () => {
     setData(initData);
-    setLogo({});
-    document.getElementById("logo").value = "";
+    setFile();
+    document.getElementById("banner").value = "";
+    setReloadFlag((prev) => prev + 1);
   };
 
-  async function uploadPic(pic, name, folder) {
-    const containerName = folder;
+  const isFormValid =
+  data.title.trim() !== "" &&
+  data.showdate.trim() !== "" &&
+  data.description.trim() !== "" &&
+  file !== undefined;
 
-    let key = process.env.REACT_APP_sol_exh;
 
-    const blobService = new BlobServiceClient(
-      `https://worldfair.blob.core.windows.net/?${key}`
-    );
-
-    const containerClient = blobService.getContainerClient(containerName);
-    const blobClient = containerClient.getBlockBlobClient(name);
-    const options = { blobHTTPHeaders: { blobContentType: pic.type } };
-    await blobClient.uploadData(pic, options);
-  }
-
+  // 🔹 Submit
   const submitData = async () => {
-    
-    if (data.Title === "") {
-      alert("Please input Title before save.");
-      return;
-    }
+    if (!isFormValid) return;
 
-    if (data.Text === "") {
-      alert("Please input Text before save.");
-      return;
-    }
+    try {
+      const fd = new FormData();
+      Object.entries(data).forEach(([key, value]) => {
+        fd.append(key, value);
+      });
+      if (file) fd.append("file", file);
 
-    if (data.Showdate === "") {
-      alert("Please input Show Date before save.");
-      return;
-    }
-   
-    if (!logo.file) {
-      alert("Please select Highlight image before save.");
-      return;
-    }
+      const res = await Axios.post(url + "/postHilight", fd);
 
-    let dat = data;
-    if (logo.file != undefined) {
-      if (logo.type != ".jpg" && logo.type != ".png") {
-        alert('Please upload logo file "jpg" or "png" format only');
-        return;
+      if (res.status === 200) {
+        alert("Add Highlight Exhibition Success");
+        setData(initData);
+        setFile();
+        document.getElementById("banner").value = "";
+        setReloadFlag((prev) => prev + 1); // 🔁 reload table
       }
-      if (logo.size > 520000) {
-        alert("Please upload file size not exceed 500KB");
-        return;
-      }
-      await uploadPic(logo.file, logo.p_name + logo.type, "solution-exlogo");
-      dat.Logo = logo.p_name + logo.type;
+    } catch (error) {
+      console.error(error);
+      alert("Submit failed");
     }
-
-    // const res = await Axios.post(url + "/addExhibition", dat).then((r) => {
-    //   if (r.status == 200) {
-    //     alert("Save success.");
-    //     clickCancel();
-    //   } else {
-    //     alert("Save failed.");
-    //   }
-    // });
   };
-
-  useEffect(() => {
-    console.log(logo);
-  }, [logo]);
 
   return (
+    <section id="createExhibition" className="xl:w-4/5 2xl:w-3/4">
+      <div className="text-xl mb-4">Highlight Exhibition</div>
 
+     
+      <div className="flex items-center gap-3 mb-3">
+        <label className="w-36">Exhibition Title</label>
+        <input
+          type="text"
+          className="md:w-96"
+          value={data.title}
+          onChange={(e) => setData({ ...data, title: e.target.value })}
+        />
+      </div>
 
-    <section id="createHighlightExhibition" className="xl:w-4/5 2xl:w-3/4">
-      {/* <div className="text-xl">Data Highlight Exhibition</div> */}
-       <div className="my-4">   
-          <Hilight/>
-        </div>
+     
+      <div className="flex items-center gap-3 mb-3">
+        <label className="w-36">Show Day</label>
+        <input
+          type="text"
+          className="md:w-96"
+          value={data.showdate}
+          onChange={(e) => setData({ ...data, showdate: e.target.value })}
+        />
+      </div>
 
-      <div className="text-xl">Highlight Exhibition</div>
-        <div className="my-4">        
-          <div className="flex items-center gap-3 mb-3 w-full">
-            <label className="w-36" htmlFor="Title">
-              Title
-            </label>
+     
+      <div className="flex items-start gap-3 mb-3">
+        <label className="w-36 mt-1">Description</label>
+        <textarea
+           className="border border-[#b3b3b3] w-96 rounded-md px-2 py-1 focus:outline-none focus:shadow-[0_0_0_0.2rem_white,0_0_5px_0.25rem_red] focus:border-white"
+          rows={3}
+          value={data.description}
+          onChange={(e) =>
+            setData({ ...data, description: e.target.value })
+          }
+        />
+      </div>     
+
+      <div className="flex items-start gap-3 mb-3 w-full">
+          <label className="w-36" htmlFor="logo">
+            Highlight file
+          </label>
+          <div>
             <input
-              type="text"
+              type="file"
               className="md:w-96"
-              id="Title"
-              onChange={(e) => setData({ ...data, Title: e.target.value })}
-              value={data.Title}
+              id="banner"
+              onChange={(e) => {
+                setFile(e.target.files[0]);
+              }}
             />
-          </div>
-          
-          <div className="flex items-center gap-3 mb-3 w-full">
-            <label className="w-36" htmlFor="Text">
-              Text
-            </label>
-            <input
-              type="text"
-              className="w-96"
-              id="Text"
-              onChange={(e) => setData({ ...data, Text: e.target.value })}
-              value={data.Text}
-            />
-          </div>
-
-          <div className="flex items-center gap-3 mb-3 w-full">
-            <label className="w-36" htmlFor="Showdate">
-              Show Date
-            </label>
-            <input
-              type="text"
-              className="md:w-96"
-              id="Showdate"
-              onChange={(e) => setData({ ...data, Showdate: e.target.value })}
-              value={data.Showdate}
-            />
-          </div>       
-        
-          <div className="flex items-start gap-3 mb-3 w-full">
-            <label className="w-36" htmlFor="logo">
-              Highlight image
-            </label>
             <div>
-              <input
-                type="file"
-                className="md:w-96"
-                id="logo"
-                onChange={(e) => {                         
-                    setLogo({
-                      ...logo,
-                      name: e.target.files[0].name,
-                      file: e.target.files[0],
-                      p_name: Date.now() + "_" + data.ExhibitionID,
-                      size: e.target.files[0].size,
-                      type: e.target.files[0].name.substring(
-                        e.target.files[0].name.lastIndexOf(".")
-                      ),
-                    });
-                  }}
-                  disabled={data.Title === ""}
-                />
-              <div>
-                {logo.file != undefined && (
-                  <button
-                    className="btn-gray px-2 mt-2"
-                    onClick={() => {
-                      setLogo({});
-                      document.getElementById("logo").value = "";
-                    }}
-                  >
-                    Cancel
-                  </button>
-                )}
-              </div>
-            </div>
-            <div className="max-w-[120px] max-h-[100px] mt-2">
-              {logo.file != undefined && (
-                <img
-                  src={URL.createObjectURL(logo.file)}
-                  alt="signature"
-                  className="object-contain object-center w-full max-h-[100px]"
-                />
+              {file != undefined && (
+                <button
+                  className="btn-gray px-2 mt-2"
+                  onClick={() => {
+                    setFile();
+                    document.getElementById("banner").value = "";
+                  }}>
+                  Cancel
+                </button>
               )}
             </div>
           </div>
-        </div>
-        <div className="flex justify-between">
-          <button className="btn-gray px-2" onClick={clickCancel}>
-            Cancel
-          </button>
-          <button className="btn-green px-4" onClick={submitData}>
-            Add
-          </button>
-        </div>
-       
+          <div className="max-w-[120px] max-h-[100px] mt-2">
+            {file != undefined && (
+              <img
+                src={URL.createObjectURL(file)}
+                alt="signature"
+                className="object-contain object-center w-full max-h-[100px]"
+              />
+            )}
+          </div>
+      </div>
+
+    
+      <div className="flex justify-between mt-4">
+        <button className="btn-gray px-2" onClick={clickCancel}>
+          Clear Data
+        </button>
+
+        <button
+          className={`px-4 btn-green ${
+            !isFormValid ? "opacity-50 cursor-not-allowed" : ""
+          }`}
+          onClick={submitData}
+          disabled={!isFormValid}
+        >
+          Add Highlight Exhibition
+        </button>
+
+      </div>
+
+ 
+      <div className="mt-10">
+        <ShowHighlight reload={reloadFlag} />
+      </div>
     </section>
   );
 }
---
+++ corporate-Frontend/src/components/solution/datawarehouse/Information/showhighlight.jsx
@@ -0,0 +1,163 @@
+import { useState, useEffect } from "react";
+import Axios from "axios";
+
+export default function ShowHighlightList(props) {
+  const url = process.env.REACT_APP_API_URI + process.env.REACT_APP_web;
+
+  const [highlightList, setHighlightList] = useState([]);
+  const [selectedIds, setSelectedIds] = useState([]); 
+  const [addhl, setAddhl] = useState([]);
+  
+  
+  
+ // console.log(highlightList);
+  console.log(selectedIds);
+  console.log(addhl);
+ 
+  useEffect(() => {
+    if (highlightList.length > 0) {
+      const defaultIds = highlightList
+        .filter(h => h.show === true)
+        .map(h => h.id);
+
+      setSelectedIds(defaultIds);
+      setAddhl(defaultIds);
+    }
+  }, [highlightList]);
+
+
+  const isChanged =
+  selectedIds.length !== addhl.length ||
+  selectedIds.some(id => !addhl.includes(id));
+
+
+  /* ================== GET LIST ================== */
+  const getList = async () => {
+    try {
+      const res = await Axios.get(url + "/HLShow");
+      if (res.status === 200) {
+        setHighlightList(res.data);
+
+        // 👉 ดึง id ทุกตัวที่ show === true
+        const showIds = res.data
+          .filter(item => item.show === true)
+          .map(item => item.id);
+
+        setSelectedIds(showIds);     
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+  
+
+  /* โหลดข้อมูลครั้งแรก + เมื่อ reload เปลี่ยน */
+  useEffect(() => {
+    getList();
+  }, [props.reload]);
+
+  
+  /* ================== SAVE ================== */
+  const saveHighlight = async () => {
+    if (!isChanged) return;
+
+    try {
+      const res = await Axios.put(
+        url + "/HLSelect/",
+        selectedIds   // ✅ ส่ง array ตรง ๆ
+      );
+
+      if (res.status === 200) {
+        alert("บันทึกสำเร็จ");
+      }
+    } catch (err) {
+      console.error(err);
+      alert("เกิดข้อผิดพลาด");
+    }
+  };
+
+
+
+  return (
+    <section className="Show Highlight Exhibition">
+      
+      <div className="flex items-center gap-4">
+        <div className="font-medium text-lg">Highlight Exhibition list</div>
+
+        <button
+          className={`px-4 btn-primary ${
+            isChanged ? "cursor-pointer" : "opacity-50 cursor-not-allowed"
+          }`}
+          disabled={!isChanged}
+          onClick={saveHighlight}
+        >
+          Save
+        </button>
+
+
+      </div>
+
+
+      <table className="w-[1024px] my-4">
+        <thead>
+          <tr className="border-b border-neutral-600">
+            <th>On Show</th>
+            <th className="border-l border-neutral-600">Highlight</th>            
+            <th className="border-l border-neutral-600">Title</th>
+            <th className="border-l border-neutral-600">Show Day</th>
+            <th className="border-l border-neutral-600">Description</th>          
+           
+          </tr>
+        </thead>
+        <tbody>
+          {highlightList.length > 0 &&
+            highlightList.map((h, i) => (
+              <tr
+                className={`border-b border-neutral-600 last:border-b-0 ${
+                  h.show ? "bg-red-100" : ""
+                }`}>
+                <td className="align-middle border-l-0 text-center">
+                  <input
+                    type="checkbox"
+                    className="size-4 accent-red-500"
+                    checked={selectedIds.includes(h.id)}
+                    onChange={(e) => {
+                      if (e.target.checked) {
+                        // เพิ่มเข้า array
+                        setSelectedIds(prev => [...prev, h.id]);
+                      } else {
+                        // ลบออกจาก array
+                        setSelectedIds(prev => prev.filter(id => id !== h.id));
+                      }
+                    }}
+                  />
+                </td>         
+
+
+                <td className="align-middle border-l border-neutral-600 text-center">                
+                    <img
+                      src={`https://worldfair.blob.core.windows.net/webcard/${h.banner}`}
+                      alt={`${h.banner}`}
+                      className="object-contain w-[120px] h-45"
+                    />                 
+                </td>
+                
+                <td className="align-middle border-l border-neutral-600 text-center">
+                  {h.title}
+                </td>
+                <td className="align-middle border-l border-neutral-600 text-center">
+                  {h.showdate}
+                </td>
+                <td className="align-middle border-l border-neutral-600 ">
+                  {h.descp}
+                </td>               
+                
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </section>
+  );
+}
